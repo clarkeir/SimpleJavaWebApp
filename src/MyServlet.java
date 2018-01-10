ed0dea787fc4234e25ec09bbe39a1e243ff9e640
Iain Clarke <iainclarke@Iains-MacBook-Air.local>
2018-01-10 07:31:22 +0000

    Initial commit

---
+++ src/MyServlet.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+@javax.servlet.annotation.WebServlet(name = "MyServlet")
+
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        try (PrintWriter writer = response.getWriter()) {
+
+            writer.println("<!DOCTYPE html><html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\" />");
+            writer.println("<title>MyServlet.java:doGet(): Servlet code!</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+
+            writer.println("<h1>This is a simple java servlet.</h1>");
+
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: iainclarke
+  Date: 09/01/2018
+  Time: 15:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <h1>Simple Java Web App Demo</h1>
+  <p>To invoke the java servlet click <a href="MyServlet">here</a></p>
+  $END$
+  </body>
+</html>
